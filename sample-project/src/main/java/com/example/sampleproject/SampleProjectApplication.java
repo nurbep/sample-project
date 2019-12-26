fd1f9517693b945bcdc32a28804ed77ac2575090
Nuruzzaman Bepari <32721087+nurbep@users.noreply.github.com>
2019-12-25 22:13:43 -0500

    Add files via upload

---
+++ sample-project/src/main/java/com/example/sampleproject/SampleProjectApplication.java
@@ -0,0 +1,30 @@
+package com.example.sampleproject;
+
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectApplication.class, args);
+	}
+
+}
+
+
+@RestController
+class HelloDockerRestController {
+	@RequestMapping("/hello/{name}")
+	public String helloDocker(@PathVariable(value = "name") String name) {
+		String response = "Hello " + name + " Response received on : " + new Date();
+		System.out.println(response);
+		return response;
+
+	}
+}
